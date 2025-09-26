b1d3e39dba2ec61380006a4579b932d112d18ea9
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-26 18:59:52 +0530

    added Day2_ConditionalStatements.js

---
+++ Day2_ConditionalStatements.js
@@ -0,0 +1,37 @@
+
+//if and else
+let age =25;
+if(age>=18){
+    console.log("you can vote..")
+}
+else{
+    console.log("you cannot vote..")
+}
+
+// else if
+
+let marks =85;
+if(marks>=90){
+    console.log("A Grade");
+}
+else if (marks>=80){
+    console.log("B Grade");
+}
+
+else{
+    console.log("C Grade ");
+}
+
+// Switch case
+
+let day = "Monday"; 
+switch (day) { 
+case "Monday": 
+console.log("Start of week!"); // Output: Start of week! 
+break; 
+case "Friday": 
+console.log("Weekend soon!"); 
+break; 
+default: 
+console.log("Some other day!"); 
+}
