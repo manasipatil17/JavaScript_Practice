d4a15044530d80546d53ae070cfd84dab275515b
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-26 19:05:15 +0530

    added Day2_Loops.js

---
+++ Day2_Loops.js
@@ -0,0 +1,21 @@
+//for loop
+
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+
+//while loop
+
+let count =1;
+while(count<=3){
+    console.log(count);
+    count++;
+}
+
+//for each loop
+
+let fruits =["apple", "mango", "banana"];
+fruits.forEach(function(fruit){
+    console.log(fruit);
+    
+})
