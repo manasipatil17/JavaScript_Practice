f47fceb46fe02fe320987712537b73b25920bbbd
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-27 14:19:33 +0530

    added Day6_API_Call.js

---
+++ Day6_API_Call.js
@@ -0,0 +1,34 @@
+async function getData() {
+    
+    let rawData = await fetch('https://dummyjson.com/todo/1');
+    let res = await rawData.json();
+    console.log(res);
+}
+
+//getData();
+
+console.log("---------------------try catch----------------------------------------");
+async function getData2() {
+    try{
+    let rawData = await fetch('https://dummjson.com/todo/1');
+    let res = await rawData.json();
+    console.log(res);
+}
+catch(error){
+console.log("something went wrong...");
+
+}
+}
+
+getData2();
+
+console.log("-----------------------then and catch-------------------------------------");
+function getData3(){
+    fetch('https://dummyjson.com/todo/1')
+    .then((rawData) => rawData.json())
+    .then((data) => console.log(data))
+    .catch((e) => console.log("something went wrong..."));
+
+}
+
+getData3();
