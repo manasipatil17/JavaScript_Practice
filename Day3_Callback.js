9d43beb5124c5e5952e85dc887ffeaf120dc17f0
Manasi Patil <manasipatil17052004@gmail.com>
2025-10-15 16:34:38 +0530

    added day3_callback.js

---
+++ Day3_Callback.js
@@ -0,0 +1,15 @@
+let arr = ['pune', 'delhi', 'mumbai'];
+
+arr.forEach(function printVal(val){
+    console.log(val);
+});
+
+
+console.log("-----------------------------------------------------");
+
+let arr2=[12,22,33];
+
+arr2.forEach((val) => {
+    console.log(val);
+    
+});
