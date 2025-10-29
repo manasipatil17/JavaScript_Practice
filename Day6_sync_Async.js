87344c7fd17c322ce5980805d5dc85d0bc62b8d8
Manasi Patil <manasipatil17052004@gmail.com>
2025-10-29 19:29:18 +0530

    added sync async

--- Day6_sync_Async.js
+++ Day6_sync_Async.js
@@ -1,3 +1,12 @@
+//sync
+console.log("Start");
+console.log( "progress");
+console.log("end");
+
+console.log("----------------------------------");
+
+
+//async using setTimeout()
 console.log("Started...");
 
 setTimeout(() => {
